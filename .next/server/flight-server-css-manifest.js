8d29f1e5b544414f06ad22085d9b8605b38d29bf
Muhammad Shehzad <mshehzadmshehzad113440@gmail.com>
2023-04-29 17:52:23 +0500

    all typsecript files deleted

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "E:\\dell\\Desktop\\Next JS\\reservado\\app\\layout.tsx": [
+      "E:\\dell\\Desktop\\Next JS\\reservado\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "E:\\dell\\Desktop\\Next JS\\reservado\\app\\page": [
+      "E:\\dell\\Desktop\\Next JS\\reservado\\app\\globals.css"
+    ]
+  }
+}
